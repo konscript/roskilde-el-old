66a6c147cee12d82ac34ff94b4459889b0097848
Søren Louv-Jansen <sorenlouv@gmail.com>
2010-03-31 18:42:10 +0200

    s

--- app/controllers/projects_controller.php
+++ app/controllers/projects_controller.php
@@ -3,7 +3,7 @@ class ProjectsController extends AppController {
 
 	var $name = 'Projects';
 	var $components = array('SpecificAcl', 'Email');
-        var $helpers = array('DatePicker');
+        //var $helpers = array('DatePicker');
 
 	function index() {
 		$this->set('title_for_layout', 'Mine Projekter');
