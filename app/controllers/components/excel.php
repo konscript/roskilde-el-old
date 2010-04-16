96a084cb36c1169ddbf1e7402beb4fbb0df80dec
Søren Louv-Jansen <sorenlouv@gmail.com>
2010-04-16 13:57:39 +0200

    Fixed excel export problems. Excel can now succesfully export even if no image is attach or if an image is attached but not existing

--- app/controllers/components/excel.php
+++ app/controllers/components/excel.php
@@ -2,7 +2,7 @@
 
 class ExcelComponent extends Object {
 
-    function createExcel($data){
+    function createExcel($data) {
         //App::import('Vendor', 'PHPExcel/PHPExcel');
         //App::import('Vendor', 'PHPExcel/PHPExcel/IOFactory');
         /** Error reporting */
@@ -22,12 +22,12 @@ class ExcelComponent extends Object {
 
         // Set properties
         $objPHPExcel->getProperties()->setCreator("Roskilde El WebService")
-                                     ->setLastModifiedBy("Roskilde El WebService")
-                                     ->setTitle("Roskilde el bestilling")
-                                     ->setSubject("Roskilde el bestilling for projekt")
-                                     ->setDescription("Dette dokument blev oprettet af Søren og Lasse. I er cocks. Vi rocks!")
-                                     ->setKeywords("roskilde, 2010, musik, el, bestilling, booking")
-                                     ->setCategory("roskilde");
+                ->setLastModifiedBy("Roskilde El WebService")
+                ->setTitle("Roskilde el bestilling")
+                ->setSubject("Roskilde el bestilling for projekt")
+                ->setDescription("Dette dokument blev oprettet af Søren og Lasse. I er cocks. Vi rocks!")
+                ->setKeywords("roskilde, 2010, musik, el, bestilling, booking")
+                ->setCategory("roskilde");
 
 
         /*
@@ -56,55 +56,59 @@ class ExcelComponent extends Object {
          * //nyt faneblad
          * på index(2) indsættes billedet et vilkårligt sted
          *
-         */
+        */
 
         $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('B2', 'Underholdning') //sektion
-                    ->setCellValue('E2', $data["Group"]["title"]) //gruppenavn
+            ->setCellValue('B2', 'Underholdning') //sektion
+            ->setCellValue('E2', $data["Group"]["title"]) //gruppenavn
 
-                    //projekt
-                    ->setCellValue('C9', $data["Project"]["title"]) //Projektnavn
-                    ->setCellValue('H9', $data["Project"]["items_start"]) //start dato
-                    ->setCellValue('J9', $data["Project"]["items_end"]) //slut dato
-                    ->setCellValue('A10', $data["Project"]["build_start"]) //byggestrøm periode
-                    ->setCellValue('A12', $data["Project"]["build_end"]); //billedekommentar
+            //projekt
+            ->setCellValue('C9', $data["Project"]["title"]) //Projektnavn
+            ->setCellValue('H9', $data["Project"]["items_start"]) //start dato
+            ->setCellValue('J9', $data["Project"]["items_end"]) //slut dato
+            ->setCellValue('A10', $data["Project"]["build_start"]) //byggestrøm periode
+            ->setCellValue('A12', $data["Project"]["build_end"]); //billedekommentar
 
-                    //enheder
-                    $no = 28;
-                    foreach($data["ProjectItem"] as $key=>$array){
+        //enheder
+        $no = 28;
+        foreach($data["ProjectItem"] as $key=>$array) {
 
-                    if (!$array['item_id']) {
-                        $title = $array["title"];
-                        $usage = $array["power_usage"];
-                    }else{
-                        $title = $array["Item"]["title"];
-                        $usage = $array["Item"]["power_usage"];
-                    }
-
-
-                        $objPHPExcel->setActiveSheetIndex(0)
-                        ->setCellValue('A'.$no, $title) //navn
-                        ->setCellValue('H'.$no, $usage) //watt forbrug
-                        ->setCellValue('I'.$no, '1') //antal enheder
-                        ->setCellValue('J'.$no, $usage); //watt forbrug ialt
-                        $no++;
-                    }
-
-        //create drawing - instantiate new drawing object
-        $objDrawing = new PHPExcel_Worksheet_Drawing();
-        $objDrawing->setName('Logo');
-        $objDrawing->setDescription('Logo');
-        $objDrawing->setPath('../webroot/attachments/photos/default/'.$data["Project"]["file_path"]);
-        //$objDrawing->setHeight(36);
-        $objDrawing->setCoordinates('B3');
-
-        //Set to image worksheet
-        $objPHPExcel->setActiveSheetIndex(1);
-
-        //add the above drawing to the worksheet,
-        $objDrawing->setWorksheet($objPHPExcel->getActiveSheet());
+            if (!$array['item_id']) {
+                $title = $array["title"];
+                $usage = $array["power_usage"];
+            }else {
+                $title = $array["Item"]["title"];
+                $usage = $array["Item"]["power_usage"];
+            }
 
 
+            $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue('A'.$no, $title) //navn
+                ->setCellValue('H'.$no, $usage) //watt forbrug
+                ->setCellValue('I'.$no, '1') //antal enheder
+                ->setCellValue('J'.$no, $usage); //watt forbrug ialt
+            $no++;
+        }
+
+        /** attach if image exists **/
+        $image_path = '../webroot/attachments/photos/default/'.$data["Project"]["file_path"];
+        if(is_file($image_path)) {
+
+            //create drawing - instantiate new drawing object
+            $objDrawing = new PHPExcel_Worksheet_Drawing();
+            $objDrawing->setName('Logo');
+            $objDrawing->setDescription('Logo');
+            $objDrawing->setPath($image_path);
+            //$objDrawing->setHeight(36);
+            $objDrawing->setCoordinates('B3');
+
+            //Set to image worksheet
+            $objPHPExcel->setActiveSheetIndex(1);
+
+            //add the above drawing to the worksheet,
+            $objDrawing->setWorksheet($objPHPExcel->getActiveSheet());
+
+        }
 
         // Rename sheet
         //$objPHPExcel->getActiveSheet()->setTitle('Simple');
